d818d468698a9c041a9e27adfb08779ecd52d444
anToha <antoha1990@gmail.com>
2017-11-12 18:32:42 +0200

    Add test scenarios, test scenarios registry

---
+++ VCTDD/UITestScenario.swift
@@ -0,0 +1,82 @@
+import Foundation
+import UIKit
+
+class ViewControllerTestScenario {
+    let reportEventClosure: (_ uniqueEventDescription: String)->()
+    
+    required init(reportEventClosure: @escaping (_ uniqueEventDescription: String)->()) {
+        self.reportEventClosure = reportEventClosure
+    }
+    
+    func buildViewController() -> UIViewController {
+        preconditionFailure("Implement this method in subclass.")
+    }
+}
+
+func getSubclassesOfClass(queriedClass: AnyClass) -> [AnyClass] {
+    var count = UInt32(0)
+    let classList = objc_copyClassList(&count)!
+    
+    var resultClasses = [AnyClass]()
+    
+    for i in 0 ..< Int(count) {
+        if class_getSuperclass(classList[i]) == queriedClass {
+            resultClasses.append(classList[i])
+        }
+    }
+    return resultClasses
+}
+
+class TestScenariosRegistry {
+    private let testScenariosClasses: [AnyClass]
+    
+    private(set) lazy var testScenariosNames: [String] = {
+        testScenariosClasses.map({String(describing: $0)})
+    }()
+    
+    let testScenarios: [ViewControllerTestScenario]
+    
+    init(reportEventClosure:@escaping (String) -> (), testScenariosClasses: [AnyClass]? = nil) {
+        if let testScenariosClasses = testScenariosClasses {
+            self.testScenariosClasses = testScenariosClasses
+        } else {
+            self.testScenariosClasses = getSubclassesOfClass(queriedClass: ViewControllerTestScenario.self)
+        }
+        
+        self.testScenarios = self.testScenariosClasses.lazy.map { (currentClass: AnyClass) -> ViewControllerTestScenario in
+            let viewControllerTestScenarioClass = currentClass as! ViewControllerTestScenario.Type
+            return viewControllerTestScenarioClass.init(reportEventClosure: reportEventClosure)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+class ViewController1: UIViewController {
+    typealias Props = (a: String, b: Int, buttonTouch: () -> ())
+    var props: Props = ("", 0, {})
+}
+
+
+class TestScenarioA: ViewControllerTestScenario {
+    
+    override func buildViewController() -> UIViewController {
+        let vc = ViewController1()
+        vc.props = self.props()
+        return vc
+    }
+    
+    func props() -> ViewController1.Props {
+        return ("", 0, {
+            self.reportEventClosure(#file + #function)
+        })
+    }
+    
+}
