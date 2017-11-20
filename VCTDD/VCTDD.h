4817dce5d9cf5ba041b27d3ae7bf80073ddc2ca4
anToha <antoha1990@gmail.com>
2017-11-20 18:55:59 +0200

    Add VCTDDFramework library

---
+++ VCTDD/VCTDD.h
@@ -0,0 +1,19 @@
+//
+//  VCTDD.h
+//  VCTDD
+//
+//  Created by Anton Ogarkov on 11/20/17.
+//  Copyright © 2017 Anton Ogarkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VCTDD.
+FOUNDATION_EXPORT double VCTDDVersionNumber;
+
+//! Project version string for VCTDD.
+FOUNDATION_EXPORT const unsigned char VCTDDVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCTDD/PublicHeader.h>
+
+
---
+++ VCTDDFramework/VCTDDFramework.h
@@ -0,0 +1,19 @@
+//
+//  VCTDDFramework.h
+//  VCTDDFramework
+//
+//  Created by Anton Ogarkov on 11/20/17.
+//  Copyright © 2017 Anton Ogarkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VCTDDFramework.
+FOUNDATION_EXPORT double VCTDDFrameworkVersionNumber;
+
+//! Project version string for VCTDDFramework.
+FOUNDATION_EXPORT const unsigned char VCTDDFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VCTDDFramework/PublicHeader.h>
+
+
